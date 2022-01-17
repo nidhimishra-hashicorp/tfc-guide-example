dacfaecacd40747ebf20020f3d1778406e0121d2
nidhimishra-hashicorp <86828798+nidhimishra-hashicorp@users.noreply.github.com>
2022-01-18 00:35:30 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "c5.24xlarge"
 }
 
 variable "instance_name" {
